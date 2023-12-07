726eb3178aa1275031cd249a9f0d04e9fda97391
Antoine Gagnon-Coulombe <ANGAC3@ulaval.ca>
2023-12-06 20:20:18 -0500

    Réécriture pour retirer les erreurs et avertissements. Séparation en plusieurs fichiers pour réutilisation.

---
+++ src/main_loglogit.py
@@ -0,0 +1,66 @@
+import logging
+from biogeme import models, biogeme as bio, expressions as be
+from swissmetro_preprocessing import (
+    database,
+    ASC_TRAIN, 
+    B_TIME, 
+    TRAIN_TT_SCALED, 
+    B_COST,
+    TRAIN_COST_SCALED,
+    ASC_SM,
+    SM_TT_SCALED,
+    SM_COST_SCALED,
+    ASC_CAR,
+    CAR_TT_SCALED,
+    CAR_CO_SCALED,
+    TRAIN_AV_SP,
+    CAR_AV_SP)
+
+# Definition of the utility functions
+V1 = ASC_TRAIN + \
+     B_TIME * TRAIN_TT_SCALED + \
+     B_COST * TRAIN_COST_SCALED
+V2 = ASC_SM + \
+     B_TIME * SM_TT_SCALED + \
+     B_COST * SM_COST_SCALED
+V3 = ASC_CAR + \
+     B_TIME * CAR_TT_SCALED + \
+     B_COST * CAR_CO_SCALED
+
+# Associate utility functions with the numbering of alternatives
+V = {1: V1,
+     2: V2,
+     3: V3}
+
+# Associate the availability conditions with the alternatives
+av = {1: TRAIN_AV_SP,
+      2: be.Variable('SM_AV'),
+      3: CAR_AV_SP}
+
+# Definition of the model. This is the contribution of each
+# observation to the log likelihood function.
+logprob = models.loglogit(V, av, be.Variable('CHOICE'))
+
+# Define level of verbosity
+logger = logging.getLogger('biogeme')
+# Détermine le niveau de détail retourné durant l'exécution.
+logger.setLevel(logging.INFO)
+
+# Create the Biogeme object
+biogeme = bio.BIOGEME(database, logprob)
+biogeme.modelName = "b01logit" # Nom du rapport. Arbitraire.
+biogeme.calculateNullLoglikelihood(av) # Métrique de plus. Semble pas influencer les résultats.
+
+# Estimate the parameters
+results = biogeme.estimate()
+print(results.short_summary())
+
+# Print the estimated values
+betas = results.getBetaValues()
+for k, v in betas.items():
+    print(f"{k:10}=\t{v:.3g}")
+
+# Get the results in a pandas table
+# Les résulats obtenis sont négatifs et je sais pas pourquoi. Il faudra investiguer comment interpréter les résultats.
+pandasResults = results.getEstimatedParameters() 
+print(pandasResults)
---
+++ src/swissmetro_preprocessing.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from biogeme import database as db, expressions as be
+
+
+df = pd.read_csv("data/swissmetro.dat", sep='\t')
+database = db.Database("swissmetro", df)
+# On peut obtenir un array numpy à partir de pandas avec df.values
+
+# Removing some observations can be done directly using pandas.
+remove = (((database.data.PURPOSE != 1) & (database.data.PURPOSE != 3)) | (database.data.CHOICE == 0))
+database.data.drop(database.data[remove].index, inplace=True)
+
+# Parameters to be estimated
+ASC_CAR = be.Beta('ASC_CAR', 0, None, None, 0)
+ASC_TRAIN = be.Beta('ASC_TRAIN', 0, None, None, 0)
+ASC_SM = be.Beta('ASC_SM', 0, None, None, 1) # Le 1 veut dire que la variable est figée et ne sera pas modifiée par le modèle
+B_TIME = be.Beta('B_TIME', 0, None, None, 0)
+B_COST = be.Beta('B_COST', 0, None, None, 0)
+
+# Definition of new variables
+SM_COST = be.Variable('SM_CO') * (be.Variable('GA') == 0)
+TRAIN_COST = be.Variable('TRAIN_CO') * (be.Variable('GA') == 0)
+
+# Definition of new variables: adding columns to the database
+CAR_AV_SP = database.DefineVariable('CAR_AV_SP', be.Variable('CAR_AV') * (be.Variable('SP') != 0))
+TRAIN_AV_SP = database.DefineVariable('TRAIN_AV_SP', be.Variable('TRAIN_AV') * (be.Variable('SP') != 0))
+TRAIN_TT_SCALED = database.DefineVariable('TRAIN_TT_SCALED', be.Variable('TRAIN_TT') / 100.0)
+TRAIN_COST_SCALED = database.DefineVariable('TRAIN_COST_SCALED', TRAIN_COST / 100)
+SM_TT_SCALED = database.DefineVariable('SM_TT_SCALED', be.Variable('SM_TT') / 100.0)
+SM_COST_SCALED = database.DefineVariable('SM_COST_SCALED', SM_COST / 100)
+CAR_TT_SCALED = database.DefineVariable('CAR_TT_SCALED', be.Variable('CAR_TT') / 100)
+CAR_CO_SCALED = database.DefineVariable('CAR_CO_SCALED', be.Variable('CAR_CO') / 100)
