7c16170d75ef4b3812ecad0769607422e00410f2
lgreiner <greiner.louis@gmail.com>
2023-12-16 15:34:01 -0500

    add model 3 wip

--- src/modele_antoine.py
+++ src/modele_antoine.py
@@ -35,6 +35,24 @@ class Model1(torch.nn.Module):
         self.B_TIME = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
         self.B_COST = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
 
+    def forward(self, x):
+        V1 = self.ASC_TRAIN + self.B_TIME * x[:, 19] + self.B_COST * x[:, 20]
+        V2 = self.ASC_SM + self.B_TIME * x[:, 22] + self.B_COST * x[:, 23]
+        V3 = self.ASC_CAR + self.B_TIME * x[:, 25] + self.B_COST * x[:, 26]
+        y = x[:, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,21,24]] # enlève col TRAIN [19, 20], SM [22, 23], CAR [25, 26]
+        V = torch.concat((y, V1.unsqueeze(1), V2.unsqueeze(1), V3.unsqueeze(1)), dim=1)
+
+        return V
+    
+class Model2(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.ASC_TRAIN = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+        self.ASC_SM = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+        self.ASC_CAR = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+        self.B_TIME = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+        self.B_COST = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+
         self.linear1 = torch.nn.Linear(30, 120) # 27 variables en entrée
         self.linear2 = torch.nn.Linear(120, 60) # 3 classes de sortie
         self.linear3 = torch.nn.Linear(60, 3) # 3 classes de sortie
@@ -45,21 +63,103 @@ class Model1(torch.nn.Module):
 
     def forward(self, x):
         V1 = self.ASC_TRAIN + self.B_TIME * x[:, 19] + self.B_COST * x[:, 20]
-        V1 = V1.unsqueeze(1)
         V2 = self.ASC_SM + self.B_TIME * x[:, 22] + self.B_COST * x[:, 23]
-        V2 = V2.unsqueeze(1)
         V3 = self.ASC_CAR + self.B_TIME * x[:, 25] + self.B_COST * x[:, 26]
-        V3 = V3.unsqueeze(1) # marche pas
-        # V = torch.stack((V1, V2, V3), dim=1)
-        V = torch.stack((x, V1, V2, V3), dim=1)
+        y = x[:, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,21,24]] # enlève col TRAIN [19, 20], SM [22, 23], CAR [25, 26]
+
+        V = torch.concat((y, V1.unsqueeze(1), V2.unsqueeze(1), V3.unsqueeze(1)), dim=1)
 
         y = self.dropout1(self.relu(self.linear1(V)))
         y = self.dropout2(self.relu(self.linear2(y)))
-
-        # probabilities = F.softmax(V, dim=1)
-        # return probabilities
     
         return self.linear3(y)
+
+ 
+class Model3(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.ASC_TRAIN = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+        self.ASC_SM = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+        self.ASC_CAR = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+        self.B_TIME = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+        self.B_COST = torch.nn.Parameter(torch.randn(1, dtype=torch.float, device=DEVICE), requires_grad=True)
+
+        self.theta_parameters = torch.nn.ParameterList()
+        self.theta_parameter1 = torch.nn.Parameter(torch.randn(24,24), requires_grad=True)
+        self.theta_parameter2 = torch.nn.Parameter(torch.randn(24,24), requires_grad=True)
+        self.theta_parameters.append(self.theta_parameter1)
+        self.theta_parameters.append(self.theta_parameter2)
+
+        self.linear1 = torch.nn.Linear(24, 24) # 27 variables en entrée
+        self.linear2 = torch.nn.Linear(24, 24) # 3 classes de sortie
+        self.linear3 = torch.nn.Linear(24, 3) # 3 classes de sortie
+
+        self.relu = torch.nn.LeakyReLU()
+        self.dropout1 = torch.nn.Dropout(0.5)
+        self.dropout2 = torch.nn.Dropout(0.3)
+
+    def forward(self, x):
+        # residual = x
+
+        V1 = self.ASC_TRAIN + self.B_TIME * x[:, 19] + self.B_COST * x[:, 20]
+        V2 = self.ASC_SM + self.B_TIME * x[:, 22] + self.B_COST * x[:, 23]
+        V3 = self.ASC_CAR + self.B_TIME * x[:, 25] + self.B_COST * x[:, 26]
+        y = x[:, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,21,24]] # enlève col TRAIN [19, 20], SM [22, 23], CAR [25, 26]
+
+        V = torch.concat((y, V1.unsqueeze(1), V2.unsqueeze(1), V3.unsqueeze(1)), dim=1)
+
+        out = self.linear1(V)
+        out = F.softplus(torch.matmul(out, self.theta_parameter1))
+        out = self.linear2(out)
+        out += V
+
+        out = self.linear3(out)
+        
+        return V
+
+        # y = self.dropout1(self.relu(self.linear1(V)))
+        # y = self.dropout2(self.relu(self.linear2(y)))
+
+        # sum = 0
+        # for theta_parameter in self.theta_parameters:
+        #     V1 = F.softplus(torch.matmul(V1, self.theta_parameter))
+        #     V2 = torch.nn.functional.softplus(V2 + torch.matmul(theta_parameter, V2))
+        #     V3 = torch.nn.functional.softplus(V3 + torch.matmul(theta_parameter, V3))
+        #     new_V = F.softplus(torch.matmul())
+
+        # x += residual - sum
+    
+        # return self.linear3(y)
+
+# class ResidualBlock(nn.Module):
+#     def __init__(self, input_size, hidden_size):
+#         super(ResidualBlock, self).__init__()
+
+#         # Couche entièrement connectée 1
+#         self.fc1 = nn.Linear(input_size, hidden_size)
+#         self.bn1 = nn.BatchNorm1d(hidden_size)
+#         self.relu = nn.ReLU()
+
+#         # Couche entièrement connectée 2
+#         self.fc2 = nn.Linear(hidden_size, input_size)
+#         self.bn2 = nn.BatchNorm1d(input_size)
+
+#     def forward(self, x):
+#         # Passage à travers la première couche entièrement connectée
+#         out = self.fc1(x)
+#         out = self.bn1(out)
+#         out = self.relu(out)
+
+#         # Passage à travers la deuxième couche entièrement connectée
+#         out = self.fc2(out)
+#         out = self.bn2(out)
+
+#         # Connexion résiduelle
+#         out += x
+#         out = self.relu(out)
+
+#         return out
+
 
 if __name__ == '__main__':
     # Preparation données
@@ -70,7 +170,7 @@ if __name__ == '__main__':
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size)
 
     # Initialisation modèle
-    model = Model1()
+    model = Model3()
     # model = SwissmetroModel()
     model.to(DEVICE)
 
